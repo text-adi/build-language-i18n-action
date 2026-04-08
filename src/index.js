0e1c3dee0f4ca06a6f20df05d84439e5aeaa12f0
text-adi <alosha123456789049@gmail.com>
2026-04-08 13:01:47 +0300

    Create func compose files po

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+/**
+ * The entrypoint for the action. This file simply imports and runs the action's
+ * main logic.
+ */
+import { run } from './main.js'
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  core.info(`Hello ${nameToGreet}!`);
-
-  // Get the current time and set it as an output variable
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  core.info(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+/* istanbul ignore next */
+run()
